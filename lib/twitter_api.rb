8de44e97d653a7bd3aef39978761fef657561241
Dave Guymon <davidwguymon@gmail.com>
2020-08-12 21:12:30 -0600

    Adds caching and vice president candidate tweets.

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -1,10 +1,18 @@
 class TwitterApi
-  def self.trump_tweets
-    client.user_timeline('realDonaldTrump', count: 25, include_rts: false)
+  def self.biden_tweets
+    client.user_timeline('JoeBiden', count: 25, exclude_replies: true, include_rts: false)
   end
 
-  def self.biden_tweets
-    client.user_timeline('JoeBiden', count: 25, include_rts: false)
+  def self.harris_tweets
+    client.user_timeline('KamalaHarris', count: 25, exclude_replies: true, include_rts: false)
+  end
+
+  def self.pence_tweets
+    client.user_timeline('Mike_Pence', count: 25, exclude_replies: true, include_rts: false)
+  end
+
+  def self.trump_tweets
+    client.user_timeline('realDonaldTrump', count: 25, exclude_replies: true, include_rts: false)
   end
 
   def self.client
