d17c97ce332ccd73ba8185c9a7ec088376ecf6fb
Dave Guymon <davidwguymon@gmail.com>
2020-08-13 21:55:36 -0600

    Applies new styling to cards and text_to_true_link helper

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,3 +1,9 @@
 module TweetsHelper
+  def text_to_true_link(tweet_text)
+    URI.extract(tweet_text, ['http', 'https']).each do |uri|
+        tweet_text = tweet_text.gsub( uri, link_to(uri, uri, target: "_blank"))
+    end
 
+    simple_format(tweet_text, {}, class: "comment-body", sanitize: false)
+  end
 end
